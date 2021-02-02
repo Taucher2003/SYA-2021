d572c3d0825171cc014e3fbd88274d6c1deaff05
Taucher2003 <mc.taucher2003@gmail.com>
2021-02-02 21:07:21 +0100

    Reformatted code

--- 2021-02-02_Timer/2021-02-02_Timer/Timer.cs
+++ 2021-02-02_Timer/2021-02-02_Timer/Timer.cs
@@ -2,44 +2,37 @@
 using System.Timers;
 using System.Windows.Forms;
 
-namespace _2021_02_02_Timer
-{
-    public partial class Timer : Form
-    {
+namespace _2021_02_02_Timer {
+    public partial class Timer : Form {
         private readonly System.Timers.Timer _timer = new System.Timers.Timer();
         private double _start;
 
-        public Timer()
-        {
+        public Timer() {
             InitializeComponent();
             _timer.AutoReset = true;
             _timer.Interval = 1;
             _timer.Elapsed += TimerElapsed;
         }
 
-        private void buttonStart_Click(object sender, EventArgs e)
-        {
+        private void buttonStart_Click(object sender, EventArgs e) {
             _start = DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds;
             _timer.Start();
             buttonStart.Text = "Reset";
         }
 
-        private void buttonStop_Click(object sender, EventArgs e)
-        {
+        private void buttonStop_Click(object sender, EventArgs e) {
             if (!_timer.Enabled) return;
             _timer.Stop();
             buttonStart.Text = "Reset + Start";
         }
 
-        private void TimerElapsed(object sender, ElapsedEventArgs e)
-        {
+        private void TimerElapsed(object sender, ElapsedEventArgs e) {
             double millisDifference = DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds - _start;
             String format = formatMillis(millisDifference);
             labelOutput.Invoke(new Action(delegate() { labelOutput.Text = format; }));
         }
 
-        private String formatMillis(double millisDifference)
-        {
+        private String formatMillis(double millisDifference) {
             var millis = (long) millisDifference;
             var seconds = millis / 1000;
             var minutes = seconds / 60;
