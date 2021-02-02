83b4bcb8db5e58f3e30e78e4ad39e0fc4e7520d6
Taucher2003 <mc.taucher2003@gmail.com>
2021-02-02 17:51:48 +0100

    Renamed Form1 to Timer

--- 2021-02-02_Timer/2021-02-02_Timer/Program.cs
+++ 2021-02-02_Timer/2021-02-02_Timer/Program.cs
@@ -13,7 +13,7 @@ namespace _2021_02_02_Timer {
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Timer());
         }
     }
 }
