f48ff2360bddf0f279e75339bae855df081b60ca
Taucher2003 <mc.taucher2003@gmail.com>
2021-02-02 12:33:31 +0100

    Added timer task

---
+++ 2021-02-02_Timer/2021-02-02_Timer/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace _2021_02_02_Timer {
+    partial class Form1 {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent() {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStart.Location = new System.Drawing.Point(52, 109);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(166, 32);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOutput.Location = new System.Drawing.Point(48, 53);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(332, 24);
+            this.labelOutput.TabIndex = 1;
+            this.labelOutput.Text = "Klicke Start um die Messung zu starten";
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStop.Location = new System.Drawing.Point(224, 109);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(166, 32);
+            this.buttonStop.TabIndex = 2;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 169);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label labelOutput;
+        private System.Windows.Forms.Button buttonStop;
+    }
+}
+
---
+++ 2021-02-02_Timer/2021-02-02_Timer/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace _2021_02_02_Timer {
+    public partial class Form1 : Form {
+        private System.Timers.Timer timer = new System.Timers.Timer();
+        private double start;
+
+        public Form1() {
+            InitializeComponent();
+            timer.AutoReset = true;
+            timer.Interval = 1;
+            timer.Elapsed += timerElapsed;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e) {
+            start = DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds;
+            timer.Start();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e) {
+            timer.Stop();
+        }
+
+        private void timerElapsed(object sender, ElapsedEventArgs e) {
+            double millisDifference = DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds - start;
+            String format = formatMillis(millisDifference);
+            labelOutput.Invoke(new Action(delegate () {
+                labelOutput.Text = format;
+            }));
+        }
+
+        private String formatMillis(double millisDifference) {
+
+            long millis = (long) millisDifference;
+            long seconds = millis / 1000;
+            long minutes = seconds / 60;
+
+            millis %= 1000;
+            seconds %= 60;
+
+            String format = "";
+            format += minutes + " Minute" + (minutes != 1 ? "n" : "") + ", ";
+            format += seconds + " Sekunde" + (seconds != 1 ? "n" : "") + ", ";
+            format += millis + " Millisekunden";
+
+            return format;
+        }
+    }
+}
