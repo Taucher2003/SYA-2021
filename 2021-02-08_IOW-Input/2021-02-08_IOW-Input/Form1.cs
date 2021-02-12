4b31b4335b1d6ddf0a8ba326dea4bf57f0450f57
Taucher2003 <mc.taucher2003@gmail.com>
2021-02-12 10:02:53 +0100

    Fixed instant crash

--- 2021-02-08_IOW-Input/2021-02-08_IOW-Input/Form1.cs
+++ 2021-02-08_IOW-Input/2021-02-08_IOW-Input/Form1.cs
@@ -11,7 +11,7 @@ namespace _2021_02_08_IOW_Input {
             get => _handle;
             set {
                 _handle = value;
-                if(!_handles.Contains(value))
+                if (!_handles.Contains(value))
                     _handles.Add(value);
             }
         }
@@ -20,28 +20,29 @@ namespace _2021_02_08_IOW_Input {
         private readonly List<int> _handles = new List<int>();
 
         private readonly Timer _timer = new Timer();
-        
+
         public Form1() {
             InitializeComponent();
+            FormClosed += OnClose;
             OpenWarrior();
             _timer.Interval = 50;
             _timer.AutoReset = true;
             _timer.Elapsed += OnTimerElapsed;
             _timer.Start();
         }
-        
+
         [DllImport("iowkit", SetLastError = true)]
         public static extern int IowKitOpenDevice();
-        
+
         [DllImport("iowkit", SetLastError = true)]
         public static extern void IowKitCloseDevice(int iowHandle);
-        
+
         [DllImport("iowkit", SetLastError = true)]
         private static extern int IowKitGetNumDevs();
-        
+
         [DllImport("iowkit", SetLastError = true)]
         private static extern int IowKitGetDeviceHandle(int numDevice);
-        
+
         [DllImport("iowkit", SetLastError = true)]
         private static extern int IowKitWrite(int iowHandle, int numPipe, ref byte[] buffer, int length);
 
@@ -49,37 +50,41 @@ namespace _2021_02_08_IOW_Input {
         public static extern int IowKitReadNonBlocking(int iowHandle, int numPipe, ref byte[] buffer, int length);
 
         private void OpenWarrior() {
+            IowKitOpenDevice();
             for (var i = 1; i <= IowKitGetNumDevs(); i++) {
                 Handle = IowKitGetDeviceHandle(i);
             }
+            Console.WriteLine("HANDLE: " + Handle);
         }
-        
+
+        public byte[] data = new byte[5];
+
         private void InitializeWarriorStates(object sender, EventArgs e) {
-            var data = new byte[5];
             data[0] = 0;
-            data[1] = 1 << 5 | 1 << 6 | 1 << 7;
+            data[1] = 0xE0;
             data[2] = 0;
             data[3] = 0;
-            data[4] = 0;
             var writtenBytes = IowKitWrite(Handle, 0, ref data, 5);
-            Console.WriteLine(writtenBytes);
+            Console.WriteLine("WRITE: " + writtenBytes);
         }
+        
+        
 
-        private void OnClick(int button, bool active) {
-            
-        }
+        private void OnClick(int button, bool active) { }
 
         private void OnClose(object sender, FormClosedEventArgs e) {
+            _timer.Stop();
+
             foreach (var handle in _handles) {
                 IowKitCloseDevice(handle);
             }
-            _timer.Stop();
         }
-
+        
+        public byte[] buffer = new byte[5];
+        
         private void OnTimerElapsed(object sender, ElapsedEventArgs e) {
-            var buffer = new byte[5];
             IowKitReadNonBlocking(Handle, 0, ref buffer, 5);
-            Console.WriteLine(string.Join(" ", buffer));
+            Console.WriteLine("READ: " + string.Join(" ", buffer) + " | " + buffer.ToString());
         }
     }
 }
