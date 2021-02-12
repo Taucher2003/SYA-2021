db90c6665781eadf129b4b8f5708502661a44387
Taucher2003 <mc.taucher2003@gmail.com>
2021-02-12 11:30:57 +0100

    Completely finished IowInput Project

--- 2021-02-08_IOW-Input/2021-02-08_IOW-Input/Form1.Designer.cs
+++ 2021-02-08_IOW-Input/2021-02-08_IOW-Input/Form1.Designer.cs
@@ -19,23 +19,65 @@
         #region Vom Windows Form-Designer generierter Code
 
         /// <summary>
-        /// Erforderliche Methode für die Designerunterstützung.
-        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.labelCurrentInput = new System.Windows.Forms.Label();
+            this.panelPin15 = new System.Windows.Forms.Panel();
+            this.panelPin16 = new System.Windows.Forms.Panel();
+            this.panelPin17 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // labelCurrentInput
+            // 
+            this.labelCurrentInput.Location = new System.Drawing.Point(128, 114);
+            this.labelCurrentInput.Name = "labelCurrentInput";
+            this.labelCurrentInput.Size = new System.Drawing.Size(210, 29);
+            this.labelCurrentInput.TabIndex = 0;
+            this.labelCurrentInput.Text = "label1";
+            // 
+            // panelPin15
+            // 
+            this.panelPin15.Location = new System.Drawing.Point(128, 210);
+            this.panelPin15.Name = "panelPin15";
+            this.panelPin15.Size = new System.Drawing.Size(128, 124);
+            this.panelPin15.TabIndex = 1;
+            // 
+            // panelPin16
+            // 
+            this.panelPin16.Location = new System.Drawing.Point(292, 210);
+            this.panelPin16.Name = "panelPin16";
+            this.panelPin16.Size = new System.Drawing.Size(128, 124);
+            this.panelPin16.TabIndex = 2;
+            // 
+            // panelPin17
+            // 
+            this.panelPin17.Location = new System.Drawing.Point(449, 210);
+            this.panelPin17.Name = "panelPin17";
+            this.panelPin17.Size = new System.Drawing.Size(128, 124);
+            this.panelPin17.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelPin17);
+            this.Controls.Add(this.panelPin16);
+            this.Controls.Add(this.panelPin15);
+            this.Controls.Add(this.labelCurrentInput);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.InitializeWarriorStates);
             this.ResumeLayout(false);
-
         }
+
+        private System.Windows.Forms.Panel panelPin16;
+        private System.Windows.Forms.Panel panelPin17;
+
+        private System.Windows.Forms.Label labelCurrentInput;
+        private System.Windows.Forms.Panel panelPin15;
 
         #endregion
     }
--- 2021-02-08_IOW-Input/2021-02-08_IOW-Input/Form1.cs
+++ 2021-02-08_IOW-Input/2021-02-08_IOW-Input/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Timers;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using Timer = System.Timers.Timer;
@@ -44,33 +45,49 @@ namespace _2021_02_08_IOW_Input {
         private static extern int IowKitGetDeviceHandle(int numDevice);
 
         [DllImport("iowkit", SetLastError = true)]
-        private static extern int IowKitWrite(int iowHandle, int numPipe, ref byte[] buffer, int length);
+        private static extern int IowKitWrite(int iowHandle, int numPipe, ref byte buffer, int length);
 
         [DllImport("iowkit", SetLastError = true)]
-        public static extern int IowKitReadNonBlocking(int iowHandle, int numPipe, ref byte[] buffer, int length);
+        public static extern int IowKitReadNonBlocking(int iowHandle, int numPipe, ref byte buffer, int length);
+
+        private const byte Pin15 = 1 << 5;
+        private const byte Pin16 = 1 << 6;
+        private const byte Pin17 = 1 << 7;
+        private const byte PinAllInput = Pin15 | Pin16 | Pin17;
 
         private void OpenWarrior() {
             IowKitOpenDevice();
             for (var i = 1; i <= IowKitGetNumDevs(); i++) {
                 Handle = IowKitGetDeviceHandle(i);
             }
-            Console.WriteLine("HANDLE: " + Handle);
         }
 
-        public byte[] data = new byte[5];
+        private readonly byte[] _data = new byte[5];
 
         private void InitializeWarriorStates(object sender, EventArgs e) {
-            data[0] = 0;
-            data[1] = 0xE0;
-            data[2] = 0;
-            data[3] = 0;
-            var writtenBytes = IowKitWrite(Handle, 0, ref data, 5);
-            Console.WriteLine("WRITE: " + writtenBytes);
+            _data[0] = 0;
+            _data[1] = PinAllInput;
+            _data[2] = 0;
+            _data[3] = 0;
+            IowKitWrite(Handle, 0, ref _data[0], 5);
         }
-        
-        
 
-        private void OnClick(int button, bool active) { }
+
+        private void OnClick(int button, bool active) {
+            var panel = button == 5
+                ? panelPin15
+                : button == 6
+                    ? panelPin16
+                    : panelPin17;
+            panel.BackColor = active ? Color.DarkGreen : Color.DarkRed;
+            UpdateLabel();
+        }
+
+        private void UpdateLabel() {
+            labelCurrentInput.Invoke(new Action(() => {
+                labelCurrentInput.Text =$@"_buffer[1] =  {_data[1]:X2}  ( " + _data[1] + @" ) ";
+            }));
+        }
 
         private void OnClose(object sender, FormClosedEventArgs e) {
             _timer.Stop();
@@ -79,12 +96,29 @@ namespace _2021_02_08_IOW_Input {
                 IowKitCloseDevice(handle);
             }
         }
-        
-        public byte[] buffer = new byte[5];
-        
+
+        private byte[] _buffer;
+        private byte _currentPressed = PinAllInput;
+
         private void OnTimerElapsed(object sender, ElapsedEventArgs e) {
-            IowKitReadNonBlocking(Handle, 0, ref buffer, 5);
-            Console.WriteLine("READ: " + string.Join(" ", buffer) + " | " + buffer.ToString());
+            if (_buffer is null) {
+                UpdateLabel();
+                _buffer = _data;
+            }
+
+            IowKitReadNonBlocking(Handle, 0, ref _buffer[0], 5);
+
+            CheckEventCall(5, Pin15);
+            CheckEventCall(6, Pin16);
+            CheckEventCall(7, Pin17);
+
+            _currentPressed = _buffer[1];
+        }
+
+        private void CheckEventCall(int button, byte pin) {
+            if (((_currentPressed & pin) == 0) ^ ((_buffer[1] & pin) == 0)) {
+                OnClick(button, (_buffer[1] & pin) == 0);
+            }
         }
     }
 }
